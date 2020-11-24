587519045757a703a42ee306490abbfc3225f265
takayg <delight-ty.341026@ezweb.ne.jp>
2020-11-25 05:27:48 +0900

    index page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "index.html"
+    queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')[0:5]
+    context_object_name = 'posts'
