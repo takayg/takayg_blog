59839ec24e1b365f1edda122b36d38e7e8d0c3d6
takayg <delight-ty.341026@ezweb.ne.jp>
2020-11-29 06:48:01 +0900

    modeify generate_secretkey

--- takayg_blog/generate_secretkey_settings.py
+++ takayg_blog/generate_secretkey_settings.py
@@ -2,4 +2,6 @@ from django.core.management.utils import get_random_secret_key
 
 secret_key = get_random_secret_key()
 text = 'SECRET_KEY = \'{0}\''.format(secret_key)
-print(text)
+
+with open('local_settings.py', 'w') as f:
+    print(text, file=f)
