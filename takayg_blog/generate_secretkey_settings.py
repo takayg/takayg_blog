5da5a7e337f1336c9d64468d285758e25d07a146
takayg <delight-ty.341026@ezweb.ne.jp>
2020-11-29 15:17:00 +0900

    secretkey and debug

--- takayg_blog/generate_secretkey_settings.py
+++ takayg_blog/generate_secretkey_settings.py
@@ -3,5 +3,6 @@ from django.core.management.utils import get_random_secret_key
 secret_key = get_random_secret_key()
 text = 'SECRET_KEY = \'{0}\''.format(secret_key)
 
-with open('local_settings.py', 'w') as f:
+with open('./local_settings.py', 'w') as f:
     print(text, file=f)
+    print('DEBUG = False', file=f)
